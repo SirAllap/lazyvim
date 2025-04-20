fed4df944568809aaf4c51d59f01a63facc5d625
SirAllap <serallapdivad@gmail.com>
2025-04-20 14:35:51 +0200

    increase lazydocker size

--- lua/plugins/lazydocker.lua
+++ lua/plugins/lazydocker.lua
@@ -1,7 +1,16 @@
 return {
 	"crnvl96/lazydocker.nvim",
 	event = "VeryLazy",
-	opts = {},
+	opts = {
+		window = {
+			settings = {
+				width = 0.9, -- 90% of the editor's width
+				height = 0.9, -- 90% of the editor's height
+				border = "rounded", -- Optional: set the border style
+				relative = "editor", -- Optional: set the window's position relative to the editor
+			},
+		},
+	},
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 	},
