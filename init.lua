186fb0ae98575853128310a7a595039dfe3985df
SirAllap <serallapdivad@gmail.com>
2025-05-10 14:36:00 +0200

    Resolved init.lua merge conflict

--- init.lua
+++ init.lua
@@ -1,27 +1,11 @@
-<<<<<<< HEAD
 if vim.loader then
-	vim.loader.enable()
+  vim.loader.enable()
 end
 
 _G.dd = function(...)
-	require("util.debug").dump(...)
+  require("util.debug").dump(...)
 end
 vim.print = _G.dd
 
 require("config.lazy")
-=======
-vim.api.nvim_echo({
-  {
-    "Do not use this repository directly\n",
-    "ErrorMsg",
-  },
-  {
-    "Please check the docs on how to get started with LazyVim\n",
-    "WarningMsg",
-  },
-  { "Press any key to exit", "MoreMsg" },
-}, true, {})
 
-vim.fn.getchar()
-vim.cmd([[quit]])
->>>>>>> upstream/main
